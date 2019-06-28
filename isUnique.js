98e09cd811180133f9346d9847b44a1b4415ecb9
Cory <coadams9@gmail.com>
2019-06-28 13:15:07 -0400

    solved isUnique algorithm

--- isUnique.js
+++ isUnique.js
@@ -1,20 +1,36 @@
-// Determine if a string has all unique characters.
-var str = 'jklm'
-// My Initial Answer
+// // Determine if a string has all unique characters.
+var str = 'jkjm'
+// // my Initial Answer
 
-// My thoughts were to first select the initial chararcter in the string
+// // my thoughts were to first select the initial chararcter in the string
 function isUnique(str) {
     var unique = true
     for (let i = 0; i < str.length; i++) {
         let test = str[i]
         // Once the first element was selelcted I wanted to compare it to every other char in the string.
-        for (let j = 1; j < str.length; j++) {
+        for (let j = i + 1; j < str.length; j++) {
+            //NEEDED TO MAKE J = NEXT CHARACTER SO IT CHECKS FOR ALL CHARACTERS EACH TIME. MY LOGIC WAS FOR THR FIRST CHARACTER ONLY BUT WHEN ALGORITHIM GOT TO THE SECOND INTEGER IT DID NOT CHACK THE FIRST ONE.
             // if the char matches any of the other chars change the value of unique to false, if not do nothing.
             test === str[j] ? unique = false : null
         }
     }
-    // Return true or false.
-    return unique
+    // console.log true or false.
+    if (unique === true) {
+        console.log('this string is unique')
+    } else {
+        console.log('this string is not unique')
+    }
 }
 
 isUnique(str)
+
+//diff solution
+// for (var i = 0; i < str.length; i++) {
+//     for (var j = i + 1; j < str.length; j++) {
+//         if (str[i] === str[j]) {
+//             return false; // if match, return false
+//         }
+//     }
+// }
+// return true; // if no match, return true
+
